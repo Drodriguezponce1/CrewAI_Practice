b2d73626affee9b0a123e3885c8dee2250a64160
Drodriguezponce1 <Danieljrod98@yahoo.com>
2025-01-22 15:18:46 -0800

    making changes to create my personal project

--- osrs_planner.py
+++ osrs_planner.py
@@ -9,6 +9,10 @@ from utils import get_openai_api_key
 openai_api_key = get_openai_api_key()
 os.environ["OPENAI_MODEL_NAME"] = 'gpt-3.5-turbo'
 
+from crewai_tools import SerperDevTool, \
+                         ScrapeWebsiteTool, \
+                         WebsiteSearchTool
+
 planner = Agent(
     role="Leveling Planner",
     goal="Plan a factual and precise ironman guide for the video game {topic}",
