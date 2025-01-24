0d66f394d9f394a9eb7a5b5e51b5cf47139081d1
Drodriguezponce1 <Danieljrod98@yahoo.com>
2025-01-24 13:58:38 -0800

    changed LLM

--- career.py
+++ career.py
@@ -7,7 +7,7 @@ import os
 from utils import get_openai_api_key 
 
 openai_api_key = get_openai_api_key()
-os.environ["OPENAI_MODEL_NAME"] = 'gpt-3.5-turbo'
+os.environ["OPENAI_MODEL_NAME"] = 'gpt-4-turbo'
 
 from crewai_tools import SerperDevTool, \
                          ScrapeWebsiteTool, \
